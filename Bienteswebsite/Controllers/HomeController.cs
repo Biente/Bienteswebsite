f165e4494e9368bddacfaabfcb8638374cd06050
MaudienSmittenaar <57255283+MaudienSmittenaar@users.noreply.github.com>
2021-03-15 12:35:46 +0100

    hij doettet!

--- Bienteswebsite/Controllers/HomeController.cs
+++ Bienteswebsite/Controllers/HomeController.cs
@@ -20,77 +20,15 @@ namespace Bienteswebsite.Controllers
         }
 
         public IActionResult Index()
-        {
-            var proucts = GetProducts();
-            return View(products);
+        {         
+            return View();
         }
 
         public IActionResult Privacy()
         {
             return View();
         }
-
-        public List<Product> GetProducts()
-        {
-            string connectionString = "Server=informatica.st-maartenscollege.nl;Port=3306;Database=110502;Uid=110502;Pwd=inf2021sql;";
-
-            
-            List<Product> products = new List<Product>();
-
-            using (MySqlConnection conn = new MySqlConnection(connectionString))
-            {
-                conn.Open();
-
-                MySqlCommand cmd = new MySqlCommand("select * from product", conn);
-
-                using (var reader = cmd.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        Product p = new Product
-                        {
-                            
-                            Id = Convert.ToInt32(reader["Id"]),
-                            Beschikbaarheid = Convert.ToInt32(reader["Beschikbaarheid"]),
-                            Naam = reader["Naam"].ToString(),
-                            Prijs = reader["Prijs"].ToString(),
-                        };
-
-                        products.Add(p);
-                    }
-                }
-            }
-
-            return products;
-        }
-
-        public List<string> GetNames()
-        {
-           
-            string connectionString = "Server=informatica.st-maartenscollege.nl;Port=3306;Database=110502;Uid=110502;Pwd=inf2021sql;";
-
-            List<string> names = new List<string>();
-
-            using (MySqlConnection conn = new MySqlConnection(connectionString))
-            {
-                conn.Open();
-
-                MySqlCommand cmd = new MySqlCommand("select * from product", conn);
-
-                using (var reader = cmd.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        string Name = reader["Naam"].ToString();
-
-                        names.Add(Name);
-                    }
-                }
-            }
-
-            return names;
-        }
-
+      
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
