841f1acd7064a73bde93ed2cf3b6a69e09cddb07
Biente <57255281+Biente@users.noreply.github.com>
2021-05-17 12:57:01 +0200

    database gekoppeld

--- Bienteswebsite/Controllers/HomeController.cs
+++ Bienteswebsite/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace Bienteswebsite.Controllers
 {
@@ -44,8 +45,45 @@ namespace Bienteswebsite.Controllers
         }
 
         public IActionResult Index()
-        {         
-            return View();
+        {
+            var names = GetNames();
+            return View(names);
+        }
+
+        public List<string> GetNames()
+        {
+            // stel in waar de database gevonden kan worden
+            string connectionString = "Server=172.16.160.21;Port=3306;Database=110417;Uid=110417;Pwd=inf2021sql;";
+
+            // maak een lege lijst waar we de namen in gaan opslaan
+            List<string> names = new List<string>();
+
+            // verbinding maken met de database
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                // verbinding openen
+                conn.Open();
+
+                // SQL query die we willen uitvoeren
+                MySqlCommand cmd = new MySqlCommand("select * from product", conn);
+
+                // resultaat van de query lezen
+                using (var reader = cmd.ExecuteReader())
+                {
+                    // elke keer een regel (of eigenlijk: database rij) lezen
+                    while (reader.Read())
+                    {
+                        // selecteer de kolommen die je wil lezen. In dit geval kiezen we de kolom "naam"
+                        string Name = reader["Naam"].ToString();
+
+                        // voeg de naam toe aan de lijst met namen
+                        names.Add(Name);
+                    }
+                }
+            }
+
+            // return de lijst met namen
+            return names;
         }
 
         public IActionResult Privacy()
